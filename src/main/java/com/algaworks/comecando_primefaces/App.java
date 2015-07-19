a7d2b74d865b269c1694f1b1d9614cbc9c3f5027
Flávio Lopes <flaviolopes.cc@gmail.com>
2015-07-19 15:46:07 -0300

    Projeto

---
+++ src/main/java/com/algaworks/comecando_primefaces/App.java
@@ -0,0 +1,13 @@
+package com.algaworks.comecando_primefaces;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Começando com PrimeFaces Workshop");
+	}
+}
